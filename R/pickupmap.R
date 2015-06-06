30d90decb5471b75e0d8012a8e421e0667bb3429
Ricebabyhaha <zwhu@ucdavis.edu>
2015-06-06 10:47:13 -0700

    better pickupmap

--- R/pickupmap.R
+++ R/pickupmap.R
@@ -2,7 +2,7 @@ starttime = Sys.time()
 
 png("pickup.png")
 nycmap <- get_map(location = as.numeric(geocode("New York")), zoom=14,maptype="roadmap", source = "google")
-ggmap(nyc)+
+ggmap(nycmap)+
   geom_point(aes(x = lon, y = lat),data = cor, size = 2, alpha = 0.1)
 dev.off()
 
---
+++ R/segmentmap.R
@@ -0,0 +1,3 @@
+http://www.geo.ut.ee/aasa/LOOM02331/R_tutorial_01.html
+
+http://flowingdata.com/2011/05/11/how-to-map-connections-with-great-circles/
